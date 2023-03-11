fc7063e930667837d3be6bb4322b9b309ecc9f08
Fabian Cnyrim <fabian@cnyrim.eu>
2023-03-11 18:03:39 +0100

    feat: multi os umlaute

--- keymaps/default/config.h
+++ keymaps/default/config.h
@@ -17,4 +17,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_MACOS, UNICODE_MODE_WINCOMPOSE
